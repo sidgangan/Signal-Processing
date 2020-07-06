705500971ec6e761d86bfc2cc401ba101ec6a4a5
sidgangan <sgangan298@gmail.com>
2020-07-06 22:36:12 +0530

    Added comments to tflite_demo

--- tflite_demo.cpp
+++ tflite_demo.cpp
@@ -7,6 +7,7 @@
 
 int main(){
 
+    // load model
     std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile("test2.tflite");
 
     if(!model){
@@ -14,17 +15,25 @@ int main(){
         exit(0);
     }
 
+    // build interpreter
     tflite::ops::builtin::BuiltinOpResolver resolver;
     std::unique_ptr<tflite::Interpreter> interpreter;
     tflite::InterpreterBuilder(*model.get(), resolver)(&interpreter);
 
-    // Resize input tensors, if desired.
+    // Resize input tensors
     interpreter->ResizeInputTensor(0,{1,129,8,1});
+
+    // Allocate tensors
     interpreter->AllocateTensors();
 
+    // pointer to input tensor of interpreter
     float* input = interpreter->typed_input_tensor<float>(0);
+
+    // create input output arrays of required dimensions
     float my_input[1][129][8][1];
     float my_output[1][129][1][1];
+
+
     // Dummy input for testing
     for(int i=0;i<129;i++){
         for(int j=0;j<8;j++){
@@ -32,14 +41,20 @@ int main(){
         }
     }
 
+
+    // copy input array to tflite input
     memcpy(input,my_input,sizeof(my_input));
 
-
+    // run model
     interpreter->Invoke();
 
+    // pointer to output tensor of interpreter
     float* output = interpreter->typed_output_tensor<float>(0);
 
+    // copy tflite output to output array
     memcpy(my_output,output,sizeof(my_output));
+
+
     printf("Result is:\n");
     for(int i=0;i<129;i++){
         for(int j=0;j<1;j++){
