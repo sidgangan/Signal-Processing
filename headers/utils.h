27475bcd1a2b64259db4337d8c27a4bbd053e0d2
sidgangan <sgangan298@gmail.com>
2020-07-08 17:21:43 +0530

    Added comments in utils.h

--- headers/utils.h
+++ headers/utils.h
@@ -12,6 +12,7 @@ void square(T* a, int size){
     }
 }
 
+// Copy one array to another
 template<class T>
 void copy_array(T* src, T* dest, int size){
     for(int i=0; i<size; i++){
@@ -19,13 +20,65 @@ void copy_array(T* src, T* dest, int size){
     }
 }
 
+/*
+Calculates mean of elements in given 2D array
+Usage: mean((double *)arr, m, n) 
+where arr is m x n 2D array
+*/
 double mean(double* arr, int m, int n);
+
+/*
+Calculates mean of squares of elements in given 2D array
+Usage: mean_square((double *)arr, m, n) 
+where arr is m x n 2D array
+*/
 double mean_square(double* arr, int m, int n);
+
+/*
+Calculates std deviation of elements in given 2D array
+Usage: std_deviation((double *)arr, m, n, mean) 
+where arr is m x n 2D array
+*/
 double std_deviation(double* arr, int m,int n, double mean);
+
+/*
+Normalize values around given mean and std deviation
+Usage: normalize((double *)arr, m, n, mean, std_dev) 
+where arr is m x n 2D array
+*/
 void normalize(double* arr, int m, int n, double mean, double std_dev);
+
+/*
+Calculates magnitude of each complex number in spectrum array
+spectrum => 1D array of complex numbers
+mag => output array storing the magnitudes
+size => length of arrays
+*/
 void magnitude(cd* spectrum, double* mag, int size);
+
+/*
+Calculates angle of each complex number in spectrum array
+spectrum => 1D array of complex numbers
+mag => output array storing the magnitudes
+size => length of arrays
+*/
 void angle(cd* spectrum, double* ang, int size);
+
+/*
+Converts complex number from polar coordinates
+mag => magnitude array
+phase => angle array
+spect => output array of complex numbers
+size => size of arrays
+*/
 void complex_from_polar(double* mag, double* phase, cd* spect, int size);
+
+/*
+Calculates FFT as well as Inverse FFT of array
+a => input array
+size => size
+invert => True/FFT , False/IFFT
+*/
 void fft_util(cd* a, int size, bool invert);
 
 
